16b82223e278b6cf449f795fa3e53a11004bc35a
xgf <xgf@xgf.local>
2017-12-27 17:12:22 +0800

    发送为难

--- XMPPDemo/IMManager.swift
+++ XMPPDemo/IMManager.swift
@@ -9,20 +9,30 @@
 import UIKit
 import XMPPFramework
 
-class IMManager: NSObject ,XMPPStreamDelegate ,XMPPRosterDelegate,XMPPRosterMemoryStorageDelegate,XMPPIncomingFileTransferDelegate{
+class IMManager: NSObject ,XMPPStreamDelegate ,XMPPRosterDelegate,XMPPRosterMemoryStorageDelegate,XMPPIncomingFileTransferDelegate,XMPPOutgoingFileTransferDelegate,UIAlertViewDelegate{
 
     static let shared = IMManager.init()
     private var stream:XMPPStream = XMPPStream.init()
     private var roster:XMPPRoster = XMPPRoster.init(rosterStorage: XMPPRosterMemoryStorage.init())
-    private let queue = DispatchQueue.init(label: "stream", qos: .default, attributes: .concurrent, autoreleaseFrequency: .workItem, target: nil)
-    //gateway
+    private var incomeFileTransfer:XMPPIncomingFileTransfer = XMPPIncomingFileTransfer.init()
+    private var outputFileTransfer:XMPPOutgoingFileTransfer = XMPPOutgoingFileTransfer.init()
+    private var friendApplyPresence:XMPPPresence?
+    
+    //request gateway
+    //then connect
     func connect() {
-        
+        //whxia
+        //iosdev
         let jid = XMPPJID(user: "iosdev", domain: "caigouku.com", resource: "jb")
         stream.hostName = "caigouku.com"
         stream.hostPort = 5222
         stream.myJID = jid
-        stream.addDelegate(self, delegateQueue: queue)
+        let streamQueue = DispatchQueue.init(label: "stream",
+                                             qos: .default,
+                                             attributes: .concurrent,
+                                             autoreleaseFrequency: .inherit,
+                                             target: nil)
+        stream.addDelegate(self, delegateQueue: streamQueue)
         
         let reconnect = XMPPReconnect.init()
         reconnect.activate(stream)
@@ -36,55 +46,186 @@ class IMManager: NSObject ,XMPPStreamDelegate ,XMPPRosterDelegate,XMPPRosterMemo
         let memerystore = XMPPRosterMemoryStorage.init()
         roster = XMPPRoster.init(rosterStorage: memerystore!)
         roster.activate(stream)
-        roster.addDelegate(self, delegateQueue: queue)
+        let rosterQueue = DispatchQueue.init(label: "roster",
+                                             qos: .default,
+                                             attributes: .concurrent,
+                                             autoreleaseFrequency: .inherit,
+                                             target: nil)
+        roster.addDelegate(self, delegateQueue: rosterQueue)
         roster.autoFetchRoster = true
         roster.autoAcceptKnownPresenceSubscriptionRequests = true//close auto accept friend apply
         
         let archivingstore = XMPPMessageArchivingCoreDataStorage.sharedInstance()
-        let archiving = XMPPMessageArchiving.init(messageArchivingStorage: archivingstore, dispatchQueue: queue)
+        let storeQueue = DispatchQueue.init(label: "store",
+                                             qos: .default,
+                                             attributes: .concurrent,
+                                             autoreleaseFrequency: .inherit,
+                                             target: nil)
+        let archiving = XMPPMessageArchiving.init(messageArchivingStorage: archivingstore, dispatchQueue: storeQueue)
         archiving?.activate(stream)
         
-        let transfer = XMPPIncomingFileTransfer.init(dispatchQueue: queue)
-        transfer.activate(stream)
-        transfer.addDelegate(self, delegateQueue: queue)
-        transfer.autoAcceptFileTransfers = true
+        let incomeFileQueue = DispatchQueue.init(label: "incomeFile",
+                                            qos: .default,
+                                            attributes: .concurrent,
+                                            autoreleaseFrequency: .inherit,
+                                            target: nil)
+        incomeFileTransfer = XMPPIncomingFileTransfer.init(dispatchQueue: incomeFileQueue)
+        incomeFileTransfer.activate(stream)
+        incomeFileTransfer.addDelegate(self, delegateQueue: incomeFileQueue)
+        incomeFileTransfer.autoAcceptFileTransfers = true
+        
+        let outgoingFileQueue = DispatchQueue.init(label: "outgoingFile",
+                                                 qos: .default,
+                                                 attributes: .concurrent,
+                                                 autoreleaseFrequency: .inherit,
+                                                 target: nil)
+        outputFileTransfer = XMPPOutgoingFileTransfer.init(dispatchQueue: outgoingFileQueue)
+        outputFileTransfer.activate(stream)
+        outputFileTransfer.addDelegate(self, delegateQueue: outgoingFileQueue)
         
         do{ try stream.connect(withTimeout: TimeInterval(30)) } catch _ {}
     }
-    func send(_ content:String) {
+}
+//MARK:send
+extension IMManager {
+    //send text
+    func send(text:String) {
         let targetId = XMPPJID(user: "whxia", domain: "caigouku.com", resource: nil)
         let message = XMPPMessage.init(type: "chat", to: targetId)
-        message.addBody(content)
+        message.addBody(text)
         stream.send(message)
     }
-    //MARK:XMPPStreamDelegate
+    //send file
+    func send(file:String) {
+        let url = URL.init(fileURLWithPath: file)
+        var data:Data? = nil
+        do{
+            try data = Data.init(contentsOf: url)
+        }catch let error {
+            print(#function + error.localizedDescription)
+        }
+        guard data != nil else {
+            return
+        }
+        //iosdev
+        //whxia
+        let targetId = XMPPJID(user: "whxia", domain: "caigouku.com", resource: nil)
+        outputFileTransfer.send(data!, toRecipient: targetId)
+    }
+}
+//MARK:connect
+extension IMManager {
+    func xmppStream(_ sender: XMPPStream, socketDidConnect socket: GCDAsyncSocket) {
+        print("socket did connect")
+    }
     func xmppStreamDidDisconnect(_ sender: XMPPStream, withError error: Error?) {
-        print("连接断开")
+        print("xml did disconnect")
     }
     func xmppStreamDidConnect(_ sender: XMPPStream) {
-        print("连接成功")
-        do{ try stream.authenticate(withPassword: "iosDev189") } catch{}
+        print("xml did connect")
+        do{
+            //iosDev189
+            //xiaCGK1991
+            try stream.authenticate(withPassword: "iosDev189")
+        } catch let error{
+            print(#function + error.localizedDescription)
+        }
     }
+}
+//MARK:message
+extension IMManager {
     func xmppStream(_ sender: XMPPStream, didReceive message: XMPPMessage) {
-        print("收到消息：\(message.name ?? "")\(message.body ?? "")")
+        print("new msg: \(message.name ?? "")\(message.body ?? "")")
     }
     func xmppStream(_ sender: XMPPStream, didSend message: XMPPMessage) {
-        print("消息发送成功")
+        print("message send success")
     }
     func xmppStream(_ sender: XMPPStream, didFailToSend message: XMPPMessage, error: Error) {
-        print("消息发送失败")
+        print("msg send error")
     }
+}
+//MARK:login
+extension IMManager {
     func xmppStreamDidAuthenticate(_ sender: XMPPStream) {
-        print("登录成功")
+        print("login success")
         let presence = XMPPPresence.init()
         presence.addChild(DDXMLNode.element(withName: "status", stringValue: "在线") as! DDXMLNode)
         presence.addChild(DDXMLNode.element(withName: "show", stringValue: "xa") as! DDXMLNode)
         stream.send(presence)
     }
     func xmppStream(_ sender: XMPPStream, didNotAuthenticate error: DDXMLElement) {
-        print("登录失败：\(error)")
+        print("login failed: \(error)")
+    }
+}
+//MARK:friends
+extension IMManager {
+    func xmppRoster(_ sender: XMPPRoster, didReceivePresenceSubscriptionRequest presence: XMPPPresence) {
+        let msg = presence.from?.bare ?? "" + "May i make a friend with you?"
+        let alert = UIAlertView.init(title: "Tip", message: msg, delegate: self, cancelButtonTitle: "Reject", otherButtonTitles: "Agree")
+        alert.show()
     }
     func xmppStream(_ sender: XMPPStream, didReceive presence: XMPPPresence) {
-        
+        guard presence.from != nil else {
+            return
+        }
+        if presence.type == "unsubscribe" {//friend delete
+            roster.removeUser(presence.from!)//remove from contact
+        }
+    }
+    func xmppRosterDidBeginPopulating(_ sender: XMPPRoster, withVersion version: String) {
+        //start sync friend list
+    }
+    func xmppRosterDidEndPopulating(_ sender: XMPPRoster) {
+        //friend sync end(has been saved in coredata)
+        NotificationCenter.default.post(Notification.init(name: Notification.Name.init("XMPPRosterDidChangeNotification")))
+    }
+    func xmppRoster(_ sender: XMPPRoster, didReceiveRosterItem item: DDXMLElement) {
+        //receive every friend
+    }
+    func xmppRosterDidChange(_ sender: XMPPRosterMemoryStorage!) {
+        NotificationCenter.default.post(Notification.init(name: Notification.Name.init("XMPPRosterDidChangeNotification")))
+    }
+    //MARK:UIAlertViewDelegate
+    func alertView(_ alertView: UIAlertView, didDismissWithButtonIndex buttonIndex: Int) {
+        guard friendApplyPresence != nil, friendApplyPresence?.from != nil else {
+            return
+        }
+        if buttonIndex == 0 {
+            roster.rejectPresenceSubscriptionRequest(from: friendApplyPresence!.from!)
+        }else{
+            roster.acceptPresenceSubscriptionRequest(from: friendApplyPresence!.from!, andAddToRoster: true)
+        }
+    }
+}
+//MARK:incoming file
+extension IMManager {
+    
+    func xmppIncomingFileTransfer(_ sender: XMPPIncomingFileTransfer!, didReceiveSIOffer offer: XMPPIQ!) {
+        sender.acceptSIOffer(offer)
+    }
+    func xmppIncomingFileTransfer(_ sender: XMPPIncomingFileTransfer!, didSucceedWith data: Data!, named name: String!) {
+        let paths: [Any] = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let url = URL(fileURLWithPath: paths.last as! String).appendingPathComponent(name)
+        do {
+            try data.write(to: url, options: [])
+        }catch let error as NSError {
+            print("Could not sendFile \(error), \(error.userInfo)")
+        }
+        print("Data was written to the path: " + url.absoluteString)
+    }
+    func xmppIncomingFileTransfer(_ sender: XMPPIncomingFileTransfer!, didFailWithError error: Error!) {
+        print("incoming file transfer failed with error:\(error.localizedDescription)")
+    }
+}
+//MARK:outgoing file
+extension IMManager {
+    func xmppOutgoingFileTransfer(_ sender: XMPPOutgoingFileTransfer!, didFailWithError error: Error!) {
+        print("outgoing file transfer failed with error:\(error.localizedDescription)")
+    }
+    func xmppOutgoingFileTransferDidSucceed(_ sender: XMPPOutgoingFileTransfer!) {
+        print("outgoinf file send success")
+    }
+    func xmppOutgoingFileTransferIBBClosed(_ sender: XMPPOutgoingFileTransfer!) {
+        print(#function)
     }
 }
--- XMPPDemo/ViewController.swift
+++ XMPPDemo/ViewController.swift
@@ -12,9 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let titles = ["text","file"]
+        for i in 0...1 {
+            let btn = UIButton.init(type: .roundedRect)
+            btn.frame = CGRect.init(x: Int(self.view.center.x - 30), y: 100+60*i, width: 60, height: 40)
+            btn.setTitle(titles[i], for: .normal)
+            btn.tag = 100 + i
+            btn.addTarget(self, action: #selector(btnDidClick(_:)), for: .touchUpInside)
+            btn.layer.cornerRadius = 4
+            btn.layer.borderColor = UIColor.lightGray.cgColor
+            btn.layer.borderWidth = 0.5
+            view.addSubview(btn)
+        }
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        IMManager.shared.send("秋名山滑板少年，你好！")
+    @objc private func btnDidClick(_ sender:UIButton) {
+        let index = sender.tag - 100
+        if index == 0 {
+            sendText()
+        }else{
+            sendFile()
+        }
+    }
+    private func sendText() {
+        IMManager.shared.send(text: "Hello，I'm iosdev -.-")
+    }
+    private func sendFile() {
+        let path = Bundle.main.path(forResource: "test", ofType: "png")
+        guard path != nil else {
+            return
+        }
+        IMManager.shared.send(file: path!)
     }
 }
 
